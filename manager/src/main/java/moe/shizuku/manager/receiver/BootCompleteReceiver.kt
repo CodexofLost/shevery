8b7d038d05c330df17032730e074b2acbfe1374d
Landon Moran <160341702+LandonMoran@users.noreply.github.com>
2026-08-15 19:34:24 -0500

    Fix typo: topjohnfu → topjohnwu

--- manager/src/main/java/moe/shizuku/manager/receiver/BootCompleteReceiver.kt
+++ manager/src/main/java/moe/shizuku/manager/receiver/BootCompleteReceiver.kt
@@ -11,7 +11,7 @@ import android.os.Build
 import android.provider.Settings
 import android.util.Log
 import androidx.annotation.RequiresApi
-import com.topjohnfu.superuser.Shell
+import com.topjohnwu.superuser.Shell
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
