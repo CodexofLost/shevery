089914d5d74a34851d85e26a98635e1b5d7f1305
Landon Moran <160341702+LandonMoran@users.noreply.github.com>
2026-08-29 11:46:56 -0500

    fix(settings): hide root boot toggle on non-rooted devices

--- manager/src/main/java/moe/shizuku/manager/settings/SettingsScreen.kt
+++ manager/src/main/java/moe/shizuku/manager/settings/SettingsScreen.kt
@@ -318,20 +318,22 @@ fun SettingsScreen() {
         item {
             SettingsGroup(title = stringResource(R.string.settings_application)) {
                 SectionHeader(stringResource(R.string.settings_startup))
-                SwitchSettingsRow(
-                    icon = R.drawable.ic_server_restart,
-                    title = stringResource(R.string.settings_start_on_boot),
-                    summary = stringResource(R.string.settings_start_on_boot_summary),
-                    checked = startOnBoot,
-                    onCheckedChange = { enabled ->
-                        ShizukuSettings.setStartOnBoot(enabled)
-                        startOnBoot = ShizukuSettings.getStartOnBoot()
-                        packageManager.setComponentEnabled(
-                            componentName,
-                            ShizukuSettings.getStartOnBoot() || ShizukuSettings.getStartOnBootAdb()
-                        )
-                    }
-                )
+                if (EnvironmentUtils.isRooted()) {
+                    SwitchSettingsRow(
+                        icon = R.drawable.ic_server_restart,
+                        title = stringResource(R.string.settings_start_on_boot),
+                        summary = stringResource(R.string.settings_start_on_boot_summary),
+                        checked = startOnBoot,
+                        onCheckedChange = { enabled ->
+                            ShizukuSettings.setStartOnBoot(enabled)
+                            startOnBoot = ShizukuSettings.getStartOnBoot()
+                            packageManager.setComponentEnabled(
+                                componentName,
+                                ShizukuSettings.getStartOnBoot() || ShizukuSettings.getStartOnBootAdb()
+                            )
+                        }
+                    )
+                }
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                     SwitchSettingsRow(
                         icon = R.drawable.ic_wadb_24,
